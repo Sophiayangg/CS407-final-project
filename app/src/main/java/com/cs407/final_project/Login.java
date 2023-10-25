ca3bd8727b8fa3f0190d2caf4e0d05eaa4e38b61
Sophia Yang <qyang254@wisc.edu>
2023-10-24 21:44:42 -0500

    finished working with signup and login page

--- app/src/main/java/com/cs407/final_project/Login.java
+++ app/src/main/java/com/cs407/final_project/Login.java
@@ -9,27 +9,25 @@ import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView loginTextView = (TextView) findViewById(R.id.login);
-        String text = "Log In";
+        setContentView(R.layout.activity_login);
+        TextView signupTextView = (TextView) findViewById(R.id.signup);
+        String text = "Sign up";
         SpannableString spannableString = new SpannableString(text);
         spannableString.setSpan(new UnderlineSpan(), 0, text.length(), 0);
-        loginTextView.setText(spannableString);
+        signupTextView.setText(spannableString);
 
-        loginTextView.setOnClickListener(new View.OnClickListener() {
+        signupTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Intent to navigate to LoginActivity
-                Intent intent = new Intent(MainActivity.this, Login.class);
+                Intent intent = new Intent(Login.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-
-
     }
 }
