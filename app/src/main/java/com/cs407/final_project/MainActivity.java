215ff804fa98108c27196f0fa9ce79182303fb11
Sophia Yang <qyang254@wisc.edu>
2023-11-02 00:08:43 -0500

    adding customize on the signup page

--- app/src/main/java/com/cs407/final_project/MainActivity.java
+++ app/src/main/java/com/cs407/final_project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cs407.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
@@ -8,10 +9,15 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,6 +56,23 @@ public class MainActivity extends AppCompatActivity {
                 String  email = String.valueOf(editTextEmail.getText());
                 String  password = String.valueOf(editTextPassword.getText());
 
+                CheckBox termsCheckbox = findViewById(R.id.checkBox); // Checkbox for terms of use
+                if (!termsCheckbox.isChecked()) {
+                    showErrorToast("You must agree to the terms of use.");
+                    //Toast.makeText(MainActivity.this, "You must agree to the terms of use.", Toast.LENGTH_LONG).show();
+
+                    return;
+                }
+
+                if (!isValidEmail(email)) {
+                    showErrorToast("Invalid email address.");
+                    return;
+                }
+
+                if (!isPasswordStrong(password)) {
+                    showErrorToast("Password does not meet requirements.");
+                    return;
+                }
                 Context context = getApplicationContext();
                 SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("users",Context.MODE_PRIVATE,null);
                 DBHelper dbHelper = new DBHelper(sqLiteDatabase);
@@ -61,4 +84,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    private boolean isValidEmail(String email) {
+        // Check if email is valid
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        }
+    private boolean isPasswordStrong(String password) {
+        // Example check for a strong password: at least one digit, one lowercase, one uppercase, one special character, and minimum 8 characters long
+        String passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        return password.matches(passwordPattern);
+    }
+
+    private void showErrorToast(String message) {
+        // Inflate the custom layout.
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast_layout,
+                (ViewGroup) findViewById(R.id.custom_toast_container)); // Replace with the ID of your container
+
+        TextView text = (TextView) layout.findViewById(R.id.toast_text);
+        text.setText(message); // Set your error message
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout); // Set the inflated layout
+        toast.show();
+    }
+
+
+
 }
