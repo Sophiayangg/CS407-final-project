f3df8e52c8d46cab1790ec3f00ae785991668d9b
Carolololol <liu966@wisc.edu>
2023-12-11 20:18:45 -0600

    previous

--- app/src/main/java/com/cs407/final_project/EditProfile.java
+++ app/src/main/java/com/cs407/final_project/EditProfile.java
@@ -1,10 +1,12 @@
 package com.cs407.final_project;
 
-import android.Manifest;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -26,55 +28,25 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import android.content.ContextWrapper;
 
-public class EditProfile extends AppCompatActivity implements View.OnClickListener {
-
+public class EditProfile extends AppCompatActivity {
     private static final int PERMISSION_CODE = 1000;
     private static final int IMAGE_CAPTURE_CODE = 1001;
     private static final int PICK_IMAGE = 1002;
-    private static final String TAG = "EditProfileActivity";
 
     private String email;
-    private Uri imageUri;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
+    private Uri image_uri;
 
-        SharedPreferences preferences = getSharedPreferences("User", MODE_PRIVATE);
-        email = preferences.getString("email", "");
 
-        ImageView profileImageView = findViewById(R.id.avatarImageView);
-        profileImageView.setOnClickListener(this);
-
-        requestPermissions();
-
-        Button saveButton = findViewById(R.id.save_btn);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateProfile();
-            }
-        });
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.avatarImageView) {
-            uploadImage();
-        }
-    }
 
     private void requestPermissions() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -87,7 +59,160 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
         }
     }
 
-    private void uploadImage() {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permissions granted
+                // You can proceed with opening the camera or gallery
+            } else {
+                // Permission was denied
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                // Handle the feature without the permission or disable it
+            }
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        SharedPreferences preferences = getSharedPreferences("User", MODE_PRIVATE);
+        email = preferences.getString("email", "");
+
+        ImageView profileImageView = findViewById(R.id.avatarImageView);
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        File mypath = new File(directory, email+".jpg");
+
+        // Check if the file exists
+        if (mypath.exists()) {
+            Bitmap retrievedBitmap = BitmapFactory.decodeFile(mypath.getAbsolutePath());
+            profileImageView.setImageBitmap(retrievedBitmap);
+        } else {
+            //default
+        }
+
+        requestPermissions();
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImagePickDialog();
+            }
+        });
+
+
+
+        Button saveButton = findViewById(R.id.svae_btn);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextFirstname = findViewById(R.id.firstName);
+                EditText editTextLastname = findViewById(R.id.lastName);
+                EditText editTextPassword = findViewById(R.id.Password);
+
+                String  firstname= String.valueOf(editTextFirstname.getText());
+                String  lastname = String.valueOf(editTextLastname.getText());
+                String  password = String.valueOf(editTextPassword.getText());
+
+
+                if (!isPasswordStrong(password)) {
+                    showErrorToast("Password must be at least 6 characters long, contain at least one number, and one lowercase letter.");
+                    return;
+                }
+
+                Context context = getApplicationContext();
+                SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("users", Context.MODE_PRIVATE, null);
+
+                DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+                dbHelper.deleteUser(email);
+                String message = dbHelper.saveUser(firstname, lastname, email, password);
+                Log.i("RegistrationTag", message);
+
+                // Check if the message indicates a duplicate email and show a Toast in that case
+                if (message.contains("email already registered")) {
+                    //Toast.makeText(MainActivity.this, "duplicated email.", Toast.LENGTH_LONG).show();
+                    showErrorToast(message);
+                } else {
+                    // Only proceed to the Home activity if the user was registered successfully
+                    Intent intent = new Intent(EditProfile.this, Profile.class);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("firstname",firstname);
+                    editor.putString("lastname",lastname);
+                    editor.apply();
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+
+    @SuppressLint("WrongConstant")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView profileImageView = findViewById(R.id.avatarImageView);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_CAPTURE_CODE) {
+            // Set captured image to the ImageButton
+            profileImageView.setImageURI(image_uri);
+            Bitmap bitmap = null;
+            try {
+                // Retrieve the image from the URI
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), image_uri);
+                // Save the image to local storage
+                saveToInternalStorage(bitmap, email);
+                profileImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            // Set picked image to the ImageButton
+            image_uri = data.getData();
+            Bitmap bitmap = null;
+            try {
+                // Retrieve the image from the URI
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), image_uri);
+                // Save the image to local storage
+                saveToInternalStorage(bitmap, email);
+                profileImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void saveToInternalStorage(Bitmap bitmapImage, String fileName) {
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        File mypath = new File(directory, fileName + ".jpg");
+        Log.e("filename1",fileName);
+        FileOutputStream fos = null;
+        if(mypath.exists()){
+            mypath.delete();
+        }
+        try {
+            fos = new FileOutputStream(mypath);
+            bitmapImage.compress(Bitmap.CompressFormat.JPEG, 60, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("profile1",e.getMessage());
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                Log.e("profile2",e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private void showImagePickDialog() {
         List<String> options = new ArrayList<>();
         if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
             options.add("Camera");
@@ -100,8 +225,10 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 if ("Camera".equals(options.get(which))) {
+                    // Camera option clicked
                     openCamera();
                 } else {
+                    // Gallery option clicked
                     openGallery();
                 }
             }
@@ -113,9 +240,9 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
         ContentValues values = new ContentValues();
         values.put(MediaStore.Images.Media.TITLE, "New Picture");
         values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
-        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
         startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
     }
 
@@ -124,106 +251,24 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
         startActivityForResult(gallery, PICK_IMAGE);
     }
 
-    @SuppressLint("WrongConstant")
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        ImageView profileImageView = findViewById(R.id.avatarImageView);
-        if (resultCode == RESULT_OK) {
-            handleImageResult(requestCode, data, profileImageView);
-        }
-    }
-
-    private void handleImageResult(int requestCode, Intent data, ImageView profileImageView) {
-        if (requestCode == IMAGE_CAPTURE_CODE) {
-            if (imageUri != null) {
-                setAndSaveImage(imageUri, profileImageView);
-            }
-        } else if (requestCode == PICK_IMAGE && data != null) {
-            imageUri = data.getData();
-            setAndSaveImage(imageUri, profileImageView);
-        }
-    }
-
-    private void setAndSaveImage(Uri uri, ImageView imageView) {
-        try {
-            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
-            imageView.setImageBitmap(bitmap);
-            saveToInternalStorage(bitmap, email);
-        } catch (IOException e) {
-            Log.e(TAG, "Error handling image result: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    private void saveToInternalStorage(Bitmap bitmapImage, String fileName) {
-        ContextWrapper cw = new ContextWrapper(getApplicationContext());
-        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
-        File mypath = new File(directory, fileName + ".jpg");
-
-        if (mypath.exists()) {
-            mypath.delete();
-        }
-
-        try (FileOutputStream fos = new FileOutputStream(mypath)) {
-            bitmapImage.compress(Bitmap.CompressFormat.JPEG, 60, fos);
-        } catch (IOException e) {
-            Log.e(TAG, "Error saving image to internal storage: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    private void updateProfile() {
-        EditText editTextFirstname = findViewById(R.id.firstName);
-        EditText editTextLastname = findViewById(R.id.lastName);
-        EditText editTextPassword = findViewById(R.id.Password);
-
-        String firstname = String.valueOf(editTextFirstname.getText());
-        String lastname = String.valueOf(editTextLastname.getText());
-        String password = String.valueOf(editTextPassword.getText());
-
-        if (!isPasswordStrong(password)) {
-            showErrorToast("Password must be at least 6 characters long, contain at least one number, and one lowercase letter.");
-            return;
-        }
-
-        Context context = getApplicationContext();
-        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("users", Context.MODE_PRIVATE, null);
-
-        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
-        dbHelper.deleteUser(email);
-        String message = dbHelper.saveUser(firstname, lastname, email, password);
-        Log.i("RegistrationTag", message);
-
-        // Check if the message indicates a duplicate email and show a Toast in that case
-        if (message.contains("email already registered")) {
-            showErrorToast(message);
-        } else {
-            Intent intent = new Intent(EditProfile.this, Profile.class);
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putString("firstname", firstname);
-            editor.putString("lastname", lastname);
-            editor.apply();
-            startActivity(intent);
-        }
-    }
-
     private boolean isPasswordStrong(String password) {
-        // Password must be at least 6 characters long, contain at least one number, and one lowercase letter
+        //  password that must be at least 6 characters long, contain at least one number, and one lowercase letter
         String passwordPattern = "^(?=.*[0-9])(?=.*[a-z]).{6,}$";
         return password.matches(passwordPattern);
     }
 
     private void showErrorToast(String message) {
+        // Inflate the custom layout.
         LayoutInflater inflater = getLayoutInflater();
-        View layout = inflater.inflate(R.layout.custom_toast_layout, (ViewGroup) findViewById(R.id.custom_toast_container));
+        View layout = inflater.inflate(R.layout.custom_toast_layout,
+                (ViewGroup) findViewById(R.id.custom_toast_container)); // Replace with the ID of your container
 
-        TextView text = layout.findViewById(R.id.toast_text);
-        text.setText(message);
+        TextView text = (TextView) layout.findViewById(R.id.toast_text);
+        text.setText(message); // Set your error message
 
         Toast toast = new Toast(getApplicationContext());
         toast.setDuration(Toast.LENGTH_LONG);
-        toast.setView(layout);
+        toast.setView(layout); // Set the inflated layout
         toast.show();
     }
 }
